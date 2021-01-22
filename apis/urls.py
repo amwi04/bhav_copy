db31ba1d7d6fc1f598fe44394570075cdc60c35d
Amod Wani <awani2@ext.uber.com>
2021-01-23 01:10:58 +0530

    apis

---
+++ apis/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+from .views import *
+
+urlpatterns = [
+    path('', index ),
+    path('get_sc/<str:sc_name>/', get_sc_name, name='get_sc_name'),
+    path('get_header/', get_header, name='get_header'),
+]
---
+++ apis/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import redis
+from django.conf import settings
+from django.http import JsonResponse
+
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                                  port=settings.REDIS_PORT, db=0)
+# Create your views here.
+
+def index(request):
+    return render(request,'apis/index.html')
+
+def get_sc_name(request,sc_name):
+    data = []
+    for i in r.scan_iter( match='*'+sc_name.upper()+'*', count=100000000):
+        data.append( r.get(i).decode().split(','))
+
+    return JsonResponse(data,safe=False)
+
+def get_header(request):
+    data = r.get('header').decode().split(',')
+    return JsonResponse(data,safe=False)
