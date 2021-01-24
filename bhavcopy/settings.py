7adcd3f6ce5b6e4c25719b40014834c95a003a6f
Amod Wani <awani2@ext.uber.com>
2021-01-25 00:37:49 +0530

    static path

--- bhavcopy/settings.py
+++ bhavcopy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = Path(BASE_DIR, 'static')
 REDIS_HOST= 'localhost'
 REDIS_PORT = 6379
 
